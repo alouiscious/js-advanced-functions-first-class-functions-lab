69281fcf999763cd5338c09b7abe4c02cb02495e
Alouiscious Media <info@reneesoul.com>
2020-03-22 03:20:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+
+
+// returnFirstTwoDrivers
+const returnFirstTwoDrivers  = function(arr) {
+  return arr.slice(0, 2)
+} 
+
+// returnLastTwoDrivers
+const returnLastTwoDrivers = (drivers) => {
+  return drivers.slice(-2)
+}
+
+// selectingDrivers
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// createFareMultiplier
+function createFareMultiplier(int) {
+  return function(times) {
+    return (int * times)
+  }
+}
+
+// fareDoubler
+const fareDoubler = (createFareMultiplier(2));
+
+// fareTripler
+const fareTripler = createFareMultiplier(3);
+
+// selectDifferentDrivers
+const selectDifferentDrivers = (drivers, selectDriver) => {
+  return selectDriver(drivers)
+}
